678e14f755bb924806506b4ec9db3ee9b9ac4a1e
Faizhal Ahmad <ahmadfaizhal8@gmail.com>
2024-03-06 09:02:04 +0700

    add all files

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed"
+import { config } from "@gluestack-ui/config"
+import { LinearGradient } from 'expo-linear-gradient';
+import Example from './component/example';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <GluestackUIProvider config={config}>
+      <Box style={styles.container}>
+        <LinearGradient
+          colors={['#48B8E9', '#48B8E9', '#BDCDD4']}
+          style={styles.container} 
+        >
+          <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+
+          <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+
+          <Example />
+
+        </LinearGradient>
+      </Box>
+
       <StatusBar style="auto" />
-    </View>
+    </GluestackUIProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    height: '100%',
+    width: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
   },
 });
